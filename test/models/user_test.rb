8a32e5d9a6ff8e1a0e163004ea29192b09b5fdee
Nicholas Major <nick@nmajor.com>
2020-09-04 10:31:27 +0100

    Updating test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "full_name returns the capitalized first name and last name" do
-    user = User.new(first_name: "john", last_name: "lennon")
-    assert_equal "John Lennon", user.full_name
+    user = User.new(first_name: "yoda", last_name: "lennon")
+    assert_equal "Yoda Lennon", user.full_name
   end
 end
